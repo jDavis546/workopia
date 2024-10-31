edcc6ce44dec32db6b989126569da58e92030ea0
Joseph Davis <josephdavis546@gmail.com>
2024-10-30 23:58:11 -0500

    Added helper functions. Refactor views.

--- helpers.php
+++ helpers.php
@@ -1,15 +1,49 @@
 <?php 
 
-/** 
+/**
  * Get the base path
- *  
+ * 
  * @param string $path
  * @return string
- * */
-
-
-function basePath($path = '') {
-    
-    return __DIR__.'/'. $path;
+ */
+function basePath($path = '')
+{
+  return __DIR__ . '/' . $path;
 }
+
+
+/**
+ * Load a view
+ * 
+ * @param string $name
+ * @return void
+ */
+function loadView($name)
+{
+  
+  $viewPath = basePath("views/{$name}.view.php");
+  if(file_exists($viewPath)){
+    require $viewPath
+  }else {
+    echo "View  {$name} not found!"
+  }
+}
+
+/**
+ * Load a partial
+ * 
+ * @param string $name
+ * @return void
+ */
+function loadPartial($name)
+{
+  $PartialPath = basePath("views/{$name}.view.php");
+  if(file_exists($PartialPath)){
+    require $PartialPath
+  }else {
+    echo "View  {$name} not found!"
+  }
+}
+}
+
 ?>
